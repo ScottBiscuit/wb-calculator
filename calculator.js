185bbf0f87a023e056aa9556f0e395350ae94229
Scott Johnstone <scottajohnstone@gmail.com>
2024-06-06 16:13:20 -0500

    Writing Functions for Operations

--- calculator.js
+++ calculator.js
@@ -1,3 +1,39 @@
+//add +
+function add(num1, num2) {
+return num1 + num2 
+}
+
+//subtract -
+function subtract(num1, num2) {
+return num1 - num2 
+}
+
+//multiply *
+function multiply(num1, num2) {
+return num1 * num2 
+}
+
+//divide /
+function divide(num1, num2) {
+return num1 / num2 
+}
+
+//power ^ 
+function power(num1, num2) {
+return num1 ** num2 
+}
+
+//mod (remainder) %
+function mod(num1, num2) {
+return num1 + num2 
+}
+
+//sqrt sqrt
+function sqrt(num1, num2) {
+return num1 ** 0.5
+}
+
+
 function calculate(expression) {}
 
 /* **************** DO NOT EDIT THE CODE BELOW **************** */
